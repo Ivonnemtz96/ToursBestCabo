b9d502ee0df12f2c73d8aaa0b7e6133b68269ddd
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-02-23 13:07:39 -0700

    Cambios en categorias-tour - Antonio

--- admin/editar/categoria-tour.php
+++ admin/editar/categoria-tour.php
@@ -1,64 +1,70 @@
-<?php 
-    require_once($_SERVER["DOCUMENT_ROOT"]."/admin/modulos/sesion.php");
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"] . "/admin/modulos/sesion.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/include/funciones.php");
 
-    if (($UserData['rol'])>2) {
-        setcookie("msg","sad",time() + 2, "/");
-        header('Location: /');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (($UserData['rol']) > 2) {
+    setcookie("msg", "sad", time() + 2, "/");
+    header('Location: /');
+}
+
+//Contadores
+$userCount   =  $db->getQueryCount('usuarios', 'id');
+$tUsuarios   = ($userCount[0]['total']);
+
+
+
+//OBTENER RANGO POR ID
+$rol = $db->getAllRecords('roles', '*', ' AND id="' . ($UserData['rol']) . '"LIMIT 1 ');
+$rol = $rol[0];
+$rol = ($rol['nombre']);
+
+
+if (isset($_REQUEST['editId']) and $_REQUEST['editId'] != "") {
+
+    $cateSel  =  $db->getAllRecords('toursCate', '*', ' AND id="' . $_REQUEST['editId'] . '"', 'LIMIT 1');
+
+    if (empty($cateSel)) { //SI NO EXISTE ES QUE NO HAY UN ID VALIDO Y REDIRECCIONAMOS Y LANZAMOS ERROR
+        setcookie("msg", "catnoen", time() + 2, "/");
+        header('location:/admin/nuevo/categoria-tour');
+        exit;
+    } else {
+        $cateSel  =  $cateSel[0];
     }
-
-    //Contadores
-    $userCount   =  $db->getQueryCount('usuarios','id');
-    $tUsuarios   = ($userCount[0]['total']);
-
-
-
-    //OBTENER RANGO POR ID
-    $rol = $db->getAllRecords('roles','*',' AND id="'.($UserData['rol']).'"LIMIT 1 ');
-    $rol = $rol[0];
-    $rol = ($rol['nombre']);
+} else {
+    setcookie("msg", "catnoen", time() + 2, "/");
+    header('location:/admin/nuevo/categoria-tour');
+    exit;
+}
 
 
-    if (isset($_REQUEST['editId']) and $_REQUEST['editId']!=""){
-    
-        $cateSel  =  $db->getAllRecords('toursCate','*',' AND id="'.$_REQUEST['editId'].'"','LIMIT 1');
-        
-            if (empty($cateSel)) { //SI NO EXISTE ES QUE NO HAY UN ID VALIDO Y REDIRECCIONAMOS Y LANZAMOS ERROR
-            setcookie("msg","catnoen",time() + 2, "/");
-                header('location:/admin/nuevo/categoria-tour');
-                exit;
-            } else {$cateSel  =  $cateSel [0];}
-        
-        }else{
-            setcookie("msg","catnoen",time() + 2, "/");
-            header('location:/admin/nuevo/categoria-tour');
-            exit;
-        }
+if (isset($_REQUEST['submit']) and $_REQUEST['submit'] != "") {
+    extract($_REQUEST);
 
-
-if(isset($_REQUEST['submit']) and $_REQUEST['submit']!=""){
-	extract($_REQUEST);
-    
-	if($descripcion=="" && $descripcion_ingles=""){
-        setcookie("msg","all",time() + 2, "/");
-		header('location:/admin/nuevo/categoria-tour');
-		exit;
-	}else{
+    if ($descripcion == "" && $descripcion_ingles = "") {
+        setcookie("msg", "all", time() + 2, "/");
+        header('location:/admin/nuevo/categoria-tour');
+        exit;
+    } else {
 
 
         $codigo = ($cateSel['imagen']); //SI NO SE SUBE LA FOTO LE DAMOS EL VALOR EXISTENTE QUE YA ESTÁ EN NUESTRA BASE DE DATOS
 
         if (!empty($_FILES['thumb']['tmp_name'])) {
-    
-            
-    
+
+
+
             $thumb = $_FILES['thumb']['tmp_name']; //DEFINIMOS LA VARIABLE THUMB YA SABEMOS QUE SI SE CARGÓ UNA FOTO
-    
+
             if ($_FILES['thumb']['type'] !== 'image/png') {
                 setcookie("msg", "fnv", time() + 2, "/");
                 header('location:/admin/editar/categoria-tour?editId=' . $_REQUEST['editId'] . '');
                 exit;
             }
-    
+
             if (($_FILES['thumb']['size']) > 1000000) {
                 setcookie("msg", "fnvz", time() + 2, "/");
                 header('location:/admin/editar/categoria-tour?editId=' . $_REQUEST['editId'] . '');
@@ -70,60 +76,44 @@ if(isset($_REQUEST['submit']) and $_REQUEST['submit']!=""){
                 $archivo = '../../upload/categorias/' . (strftime("%Y/%m", strtotime(($cateSel['fr'])))) . '/' . ($cateSel['imagen']) . '.png';
                 unlink($archivo); //BORRAMOS LA FOTO ANTIGUA SACANDO EL NOMBRE DE LA BASE DE DATOS
             }
-    
+
             $codigo =  GeraHash(10); //LO USAMOS PARA EL NOMBRE DE LA FOTO
-    
+
             $ruta = '../../upload/categorias/' . (strftime("%Y/%m", strtotime(($cateSel['fr'])))) . '';
-    
-    
+
+
             //SI LA CARPETA NO EXISTE LA CREAMOS
             if (!file_exists($ruta)) {
                 mkdir($ruta, 0777, true);
             }
-    
+
             //SUBIMOS LA FOTO EN LA CARPETA EXISTENTE O LA CREADA
             $archivo_subido = '' . $ruta . '/' . $codigo . '.png';
             move_uploaded_file($thumb, $archivo_subido);
         }
 
 
-        if(isset($_REQUEST['submit']) and $_REQUEST['submit']!=""){
+        if (isset($_REQUEST['submit']) and $_REQUEST['submit'] != "") {
             extract($_REQUEST);
-			$data	=	array(
-							'nombre'=>$descripcion,
-							'nombreIn'=>$descripcion_ingles,
-                            'imagen' => $thumb,
-						);
-	       $update	=	$db->update('toursCate',$data,array('id'=>($_REQUEST['editId'])));
-    
-            if($update){
-                setcookie("msg","carup",time() + 2, "/");
+            $data    =    array(
+                'nombre' => $descripcion,
+                'nombreIn' => $descripcion_ingles,
+                'imagen' => $codigo,
+            );
+            $update    =    $db->update('toursCate', $data, array('id' => ($_REQUEST['editId'])));
+
+            if ($update) {
+                setcookie("msg", "carup", time() + 2, "/");
                 header('location:/admin/nuevo/categoria-tour'); //Exito en el cmabio
-                exit;}
-                else{
-                    setcookie("msg","sincam",time() + 2, "/");
-                    header('location:/admin/nuevo/categoria-tour'); //sin cambios
-                    exit;
-                }
-            }
-		// 	$data	=	array(
-		// 					'nombre'=>$descripcion,
-		// 					'nombreIn'=>$descripcion_ingles,
-        //                     'imagen' => $codigo,
-		// 				);
-	    //    $update	=	$db->update('toursCate',$data,array('id'=>($cateSel['id'])));
-    
-        //     if($update){
-        //         setcookie("msg","carup",time() + 2, "/");
-        //         header('location:/admin/nuevo/categoria-tour'); //Exito en el cmabio
-        //         exit;}
-        //         else{
-        //             setcookie("msg","sincam",time() + 2, "/");
-        //             header('location:/admin/nuevo/categoria-tour'); //sin cambios
-        //             exit;
-        //         }
+                exit;
+            } else {
+                setcookie("msg", "sincam", time() + 2, "/");
+                header('location:/admin/nuevo/categoria-tour'); //sin cambios
+                exit;
             }
         }
+    }
+}
 
 ?>
 <!DOCTYPE html>
@@ -132,7 +122,7 @@ if(isset($_REQUEST['submit']) and $_REQUEST['submit']!=""){
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <?php require_once($_SERVER["DOCUMENT_ROOT"]."/admin/modulos/title.php");?>
+    <?php require_once($_SERVER["DOCUMENT_ROOT"] . "/admin/modulos/title.php"); ?>
     <!-- General CSS Files -->
     <link rel="stylesheet" href="/admin/assets/css/app.min.css">
     <link rel="stylesheet" href="/admin/assets/bundles/bootstrap-daterangepicker/daterangepicker.css">
@@ -157,8 +147,8 @@ if(isset($_REQUEST['submit']) and $_REQUEST['submit']!=""){
             <div class="navbar-bg"></div>
 
 
-            <?php require_once($_SERVER["DOCUMENT_ROOT"]."/admin/modulos/navUser.php"); ?>
-            <?php require_once($_SERVER["DOCUMENT_ROOT"]."/admin/modulos/menu-principal.php"); ?>
+            <?php require_once($_SERVER["DOCUMENT_ROOT"] . "/admin/modulos/navUser.php"); ?>
+            <?php require_once($_SERVER["DOCUMENT_ROOT"] . "/admin/modulos/menu-principal.php"); ?>
 
 
             <!-- Main Content -->
@@ -181,10 +171,10 @@ if(isset($_REQUEST['submit']) and $_REQUEST['submit']!=""){
                         <div class="row justify-content-center">
                             <div class="col-md-10">
                                 <?php
-                    //MENSAJES DE ESTATUS
-                    if(isset($_COOKIE['msg'])) {
-                        require_once($_SERVER["DOCUMENT_ROOT"]."/include/msg.php");
-                        } ?>
+                                //MENSAJES DE ESTATUS
+                                if (isset($_COOKIE['msg'])) {
+                                    require_once($_SERVER["DOCUMENT_ROOT"] . "/include/msg.php");
+                                } ?>
                             </div>
                         </div>
                     </div>
@@ -203,26 +193,20 @@ if(isset($_REQUEST['submit']) and $_REQUEST['submit']!=""){
                                             <div class="col-md-4">
                                                 <div class="form-group">
                                                     <label>Descripción</label>
-                                                    <input name="descripcion"
-                                                        value="<?php echo ($recoSel['descripcion']);?>" type="text"
-                                                        class="form-control">
+                                                    <input name="descripcion" value="<?php echo ($cateSel['nombre']); ?>" type="text" class="form-control">
                                                 </div>
                                             </div>
                                             <div class="col-md-4">
                                                 <div class="form-group">
                                                     <label>Descripción</label>
-                                                    <input name="descripcion_ingles"
-                                                        value="<?php echo ($recoSel['descripcion_ingles']);?>"
-                                                        type="text" class="form-control">
+                                                    <input name="descripcion_ingles" value="<?php echo ($cateSel['nombreIn']); ?>" type="text" class="form-control">
                                                 </div>
                                             </div>
                                             <div class="col-lg-4 col-md-4">
                                                 <div class="form-group">
                                                     <label>Imagen de ícono</label>
                                                     <div class="col-sm-12 col-md-7">
-                                                        <img class="responsive"
-                                                            src="/upload/categorias/<?php echo (strftime("%Y/%m", strtotime(($cateSel['fr'])))); ?>/<?php echo ($cateSel['imagen']) ?>.png"
-                                                            alt="">
+                                                        <img class="responsive" src="/upload/categorias/<?php echo (strftime("%Y/%m", strtotime(($cateSel['fr'])))); ?>/<?php echo ($cateSel['imagen']) ?>.png" alt="">
                                                         <div>
                                                             <label for="image-upload" id="image-label">Para reemplazar
                                                                 el ícono actual sube una nueva foto</label>
@@ -233,10 +217,8 @@ if(isset($_REQUEST['submit']) and $_REQUEST['submit']!=""){
                                             </div>
                                         </div>
                                         <div class="col-sm-2">
-                                            <a class="btn btn-primary" href="/admin/msg-estado"><i
-                                                    class="fas fa-angle-double-left"></i> Volver</a>
-                                            <button type="submit" name="submit" value="submit"
-                                                class="btn btn-primary"><i class="fas fa-save"></i> Guardar</button>
+                                            <a class="btn btn-primary" href="/admin/msg-estado"><i class="fas fa-angle-double-left"></i> Volver</a>
+                                            <button type="submit" name="submit" value="submit" class="btn btn-primary"><i class="fas fa-save"></i> Guardar</button>
 
                                         </div>
                                     </div>
@@ -253,8 +235,7 @@ if(isset($_REQUEST['submit']) and $_REQUEST['submit']!=""){
             </div>
 
             <footer class="main-footer">
-                <div class="footer-left">Copyright &copy; 2020 <div class="bullet"></div> Creado por <a target="_blank"
-                        href="http://bananagroup.mx">Banana Group</a></div>
+                <div class="footer-left">Copyright &copy; 2020 <div class="bullet"></div> Creado por <a target="_blank" href="http://bananagroup.mx">Banana Group</a></div>
                 <div class="footer-right"></div>
             </footer>
 
